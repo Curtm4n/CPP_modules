2046305515e7ac8d1f93748c242299124e1c8d2c
Curtis DAPURIFICACAO <cdapurif@e2r10p1.clusters.42paris.fr>
2022-01-12 18:25:23 +0100

    MaJ

---
+++ CPP_module00/ex01/phonebook.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdapurif <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 14:13:40 by cdapurif          #+#    #+#             */
+/*   Updated: 2022/01/12 14:15:41 by cdapurif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+
+void	ft_phonebook(void)
+{
+	std::cout << "Welcome, this is a simple phonebook" << std::endl;
+}
+
+int	main(void)
+{
+	ft_phonebook();
+	return (0);
+}
