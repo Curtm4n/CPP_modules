d276b80569b6f01328ff2e22ada60f6dfadbee88
Curtis DAPURIFICACAO <cdapurif@e2r9p4.clusters.42paris.fr>
2022-01-21 15:54:44 +0100

    Ajout de CPP03 et ex00 fini

---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdapurif <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 12:44:41 by cdapurif          #+#    #+#             */
+/*   Updated: 2022/01/21 12:54:23 by cdapurif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class	ClapTrap
+{
+
+public:
+
+	ClapTrap(std::string name);
+	ClapTrap(ClapTrap const & src);
+	~ClapTrap(void);
+
+	ClapTrap &	operator=(ClapTrap const & rhs);
+
+	void	attack(const std::string & target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+
+private:
+
+	std::string	_name;
+	int		_hitPoint;
+	int		_energyPoint;
+	int		_attackDamage;
+
+};
+
+#endif
---
+++ CPP03/ex00/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdapurif <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 12:43:56 by cdapurif          #+#    #+#             */
+/*   Updated: 2022/01/21 15:46:49 by cdapurif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap	bob("Bob");
+
+	bob.attack("Handsome Jack");
+	bob.attack("Handsome Jack");
+	bob.attack("Handsome Jack");
+	bob.attack("Handsome Jack");
+	bob.attack("Handsome Jack");
+	bob.attack("Handsome Jack");
+	bob.attack("Handsome Jack");
+	bob.attack("Handsome Jack");
+	bob.attack("Handsome Jack");
+	bob.beRepaired(1);
+	bob.beRepaired(1);
+	bob.attack("Handsome Jack");
+	bob.takeDamage(12);
+	bob.attack("Handsome Jack");
+	bob.beRepaired(1);
+	bob.takeDamage(12);
+	return (0);
+}
