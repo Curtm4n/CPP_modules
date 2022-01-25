98f5311fbf8709ab8c10ac82c528e3873aa3bac2
Curtis DAPURIFICACAO <cdapurif@e2r6p5.clusters.42paris.fr>
2022-01-25 17:17:37 +0100

    ex01 done with 2 main

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 23:14:12 by cdapurif          #+#    #+#             */
-/*   Updated: 2022/01/25 15:51:32 by cdapurif         ###   ########.fr       */
+/*   Updated: 2022/01/25 16:39:59 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,70 +19,30 @@
 
 int	main(void)
 {
-	Dog	maiya;
+	Animal	*array[10];
 
-	maiya.setBrainIdeas(0, "hello");
-	Dog	sati = maiya;
-	sati.setBrainIdeas(1, "world");
-	std::cout << sati.getBrainIdeas(0) << " " << sati.getBrainIdeas(1) << std::endl;
-	std::cout << sati.getType() << std::endl;
-
+	std::cout << Green << "Construction part" << Reset << std::endl << std::endl;
+	for (int i = 0; i < 10; i++)
+	{
+		if (i < 5)
+		{
+			std::cout << Yellow << "Creating Cat at index " << i << Reset << std::endl;
+			array[i] = new Cat();
+		}
+		else
+		{
+			std::cout << Yellow << "Creating Dog at index " << i << Reset << std::endl;
+			array[i] = new Dog();
+		}
+	}
+	std::cout << std::endl << Red << "Destruction part" << Reset << std::endl << std::endl;
+	for (int j = 0; j < 10; j++)
+	{
+		if (j < 5)
+			std::cout << Red << "Destroying Cat at index " << j << Reset << std::endl;
+		else
+			std::cout << Red << "Destroying Dog at index " << j << Reset << std::endl;
+		delete array[j];
+	}
 	return (0);
 }
-
-/*int	main(void)
-{
-	//Animal part ------------------------------------------------------------------------------
-	std::cout << Green << "Creating and testing an Animal: " << Reset << std::endl << std::endl;
-	
-	Animal			*meta = new Animal();
-	
-	std::cout << "basic noise of an Animal: ";
-	meta->makeSound();
-	std::cout << std::endl;
-	
-	std::cout << std::endl << Red << "Destroying my Animal" << Reset << std::endl;
-	delete meta;
-	
-
-	//Dog part ---------------------------------------------------------------------------------
-	std::cout << std::endl << Green << "Creating and testing a Dog: " << Reset
-		<< std::endl << std::endl;
-	
-	const Animal		*j = new Dog();
-	
-	std::cout << j->getType() << " make the sound: ";
-	j->makeSound();
-	
-	std::cout << std::endl << Red << "Destroying my Dog" << Reset << std::endl;
-	delete j;
-	
-	
-	//Cat part ----------------------------------------------------------------------------------
-	std::cout << std::endl << Green << "Creating and testing a Cat: " << Reset
-		<< std::endl << std::endl;
-	
-	const Animal		*i = new Cat();
-	
-	std::cout << i->getType() << " make the sound: ";
-	i->makeSound();
-	
-	std::cout << std::endl << Red << "Destroying my Cat" << Reset << std::endl;
-	delete i;
-	
-	
-	//WrongCat part -----------------------------------------------------------------------------
-	std::cout << std::endl << Green << "Creating and testing a WrongCat: " << Reset
-		<< std::endl << std::endl;
-	
-	const WrongAnimal	*k = new WrongCat();
-	
-	std::cout << k->getType() << " make the sound: ";
-	k->makeSound();
-	std::cout << std::endl;
-	
-	std::cout << std::endl << Red << "Destroying my WrongCat" << Reset << std::endl;
-	delete k;
-
-	return (0);
-}*/
