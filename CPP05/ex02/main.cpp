938fe61ca4979090171304255538ef83d9998d34
Curtis DAPURIFICACAO <cdapurif@e2r4p7.clusters.42paris.fr>
2022-01-31 13:08:25 +0100

    ex02 OK

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 15:10:12 by cdapurif          #+#    #+#             */
-/*   Updated: 2022/01/29 12:13:28 by cdapurif         ###   ########.fr       */
+/*   Updated: 2022/01/31 13:07:41 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 int	main(void)
 {
-	try
+	/*try
 	{
 		Bureaucrat jerry("Jerry", 3);
 		ShrubberyCreationForm a("John Wick");
@@ -29,7 +29,33 @@ int	main(void)
 	catch (std::exception & e)
 	{
 		std::cout << e.what() << std::endl;
+	}*/
+
+	try
+	{
+		Bureaucrat jerry("Jerry", 3);
+		RobotomyRequestForm a("John Wick");
+
+		jerry.signForm(a);
+		jerry.executeForm(a);
 	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	/*try
+	{
+		Bureaucrat jerry("Jerry", 3);
+		PresidentialPardonForm a("John Wick");
+
+		jerry.signForm(a);
+		jerry.executeForm(a);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}*/
 
 	return (0);
 }
