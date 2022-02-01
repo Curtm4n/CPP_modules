421ce34fdf44b3d559a871e239ce4000323b10d0
Curtis DAPURIFICACAO <cdapurif@e2r4p7.clusters.42paris.fr>
2022-02-01 14:29:09 +0100

    ex01 done

---
+++ CPP07/ex01/iter.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdapurif <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/01 13:12:51 by cdapurif          #+#    #+#             */
+/*   Updated: 2022/02/01 14:23:34 by cdapurif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+
+template<typename T>
+void	aff(T & a)
+{
+	std::cout << a << " ";
+}
+
+template<typename T>
+void	inc(T & a)
+{
+	a++;
+}
+
+template<typename T>
+void	iter(T *array, unsigned int length, void (*f)(T &))
+{
+	for (unsigned int i = 0; i < length; i++)
+	{
+		(*f)(array[i]);
+	}
+}
+
+#endif
