2d2e8ab0f2f943e8ea2e6d9378c893dc75e0d768
Curtis DAPURIFICACAO <cdapurif@e2r7p6.clusters.42paris.fr>
2022-02-03 11:07:55 +0100

    Ajout const

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 16:10:54 by cdapurif          #+#    #+#             */
-/*   Updated: 2022/02/02 17:20:07 by cdapurif         ###   ########.fr       */
+/*   Updated: 2022/02/03 11:04:23 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <algorithm>
 
 template<typename T>
-typename T::const_iterator	easyfind(T & container, int value)
+typename T::const_iterator	easyfind(T const & container, int value)
 {
 	typename T::const_iterator	it;
 	typename T::const_iterator	ite = container.end();
